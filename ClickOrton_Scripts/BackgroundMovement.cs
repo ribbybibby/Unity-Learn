99c9d2af785fd136c24da279823d23ad9940ae21
ribbybibby <robertbest89@gmail.com>
2014-11-27 00:21:45 +0000

    Updated

--- ClickOrton_Scripts/BackgroundMovement.cs
+++ ClickOrton_Scripts/BackgroundMovement.cs
@@ -3,13 +3,6 @@ using System.Collections;
 
 public class BackgroundMovement : MonoBehaviour {
 
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
 	void Update () {
 				if (transform.position.x >= 1 & transform.position.y >= -1) {
 						transform.Translate (Vector3.down * Time.deltaTime, Space.World);			
