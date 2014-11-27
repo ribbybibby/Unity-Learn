58dde13644df84b11e8d3b5e5065f510bc8e9783
Rob Best <robertbest89@gmail.com>
2014-11-27 07:42:42 +0000

    Tweaked comments

--- ClickOrton_Scripts/CenaMood.cs
+++ ClickOrton_Scripts/CenaMood.cs
@@ -9,17 +9,17 @@ public class CenaMood : MonoBehaviour {
 		GetComponent<SpriteRenderer> ().color = Color.white;
 	}
 
-	// When the amount of Ortons hits above a random number between 10 and 20 start increasing the size of the Ortons
+	// When the amount of Ortons hits above a random number between 5 and 10 start increasing the size of the Ortons
 	// each update.
-	// When the Orton count is above (10, 20)+10 and rndGrow hits 10; we swap the colour to red
+	// When the Orton count is above (5, 10)+5 and rndGrow hits 10; we swap the colour to red
 	void Update () {
 		int ortonCount = GameObject.FindGameObjectsWithTag("Ortons").Length;
-		int rndGrow = Random.Range(1,1000);
-		int rndLimit = Random.Range(10,20);
+		int rndGrow = Random.Range(1,500);
+		int rndLimit = Random.Range(5,10);
 		if (ortonCount >= rndLimit) {
 			transform.localScale += new Vector3 (0.005F, 0.005F, 0);
 		}
-		if (rndGrow == 10 & ortonCount >= rndLimit + 10) {
+		if (rndGrow == 10 & ortonCount >= rndLimit + 5) {
 			GetComponent<SpriteRenderer>().color = Color.red;
 		}					
 	}
